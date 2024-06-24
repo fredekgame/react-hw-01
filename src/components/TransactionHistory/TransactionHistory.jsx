1f7b333cefd59b51473485da4eb359f9f8a01367
Frederik Hinez <fredekgame@gmail.com>
2024-06-24 17:40:48 +0300

    upd

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,7 +1,7 @@
 import './TransactionHistory.css'
 
 const thStyles = {
-    border:" black solid 1px",
+    border: "black solid 1px",
     color: "white",
     backgroundColor: "black",
 }
@@ -11,32 +11,32 @@ const tdStyles = {
     border: "black solid 1px",
 }
 
-export const TransactionHistory = ({items}) => {
-    return(
+export const TransactionHistory = ({ items }) => {
+    return (
         <table style={{
             position: "relative",
             right: "20px",
             width: "600px",
             height: "100px",
-            borderCollapse: "collaps",
+            borderCollapse: "collapse",
             border: "black solid 1px",
         }}>
-        <thead>
-        <tr>
-            <th style={thStyles}>Type</th>
-            <th style={thStyles}>Amount</th>
-            <th style={thStyles}>Currency</th>
-        </tr>
-        </thead>
-        <tbody>
-        {items.map(({ id, type, amount, currency, index }) => (
-                <tr key={id} style={{ backgroundColor: index % 2 === 1 ? 'lightgrey' : 'white' }}>
-                   <td style={tdStyles}>{type}</td>
-                   <td style={tdStyles}>{amount}</td>
-                   <td style={tdStyles}>{currency}</td>
+            <thead>
+                <tr>
+                    <th style={thStyles}>Type</th>
+                    <th style={thStyles}>Amount</th>
+                    <th style={thStyles}>Currency</th>
                 </tr>
+            </thead>
+            <tbody>
+                {items.map(({ id, type, amount, currency }, index) => (
+                    <tr key={id} style={{ backgroundColor: index % 2 === 1 ? 'lightgrey' : 'white' }}>
+                        <td style={tdStyles}>{type}</td>
+                        <td style={tdStyles}>{amount}</td>
+                        <td style={tdStyles}>{currency}</td>
+                    </tr>
                 ))}
-        </tbody>
+            </tbody>
         </table>
     )
 }
