52904d302ec8620bfafa7eea0056fff87733b027
Frederik Hinez <fredekgame@gmail.com>
2024-06-24 17:31:44 +0300

    upd

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,31 +1,44 @@
 import './TransactionHistory.css'
 
+const thStyles = {
+    border:" black solid 1px",
+    color: "white",
+    backgroundColor: "black",
+}
+
+const tdStyles = {
+    color: "black",
+    border: "black solid 1px",
+}
+
+const trStyles = {
+    backgroundColor: "index % 2 === 1 ? 'lightgrey' : 'white'" }
 
 export const TransactionHistory = ({items}) => {
     return(
-        <table>
+        <table style={{
+            position: "relative",
+            right: "20px",
+            width: "600px",
+            height: "100px",
+            borderCollapse: "collaps",
+            border: "black solid 1px",
+        }}>
         <thead>
-        <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+        <tr style={trStyles}>
+            <th style={thStyles}>Type</th>
+            <th style={thStyles}>Amount</th>
+            <th style={thStyles}>Currency</th>
         </tr>
         </thead>
         <tbody>
         {items.map(({ id, type, amount, currency }) => (
                 <tr key={id}>
-                   <td>{type}</td>
-                   <td>{amount}</td>
-                   <td>{currency}</td>
+                   <td style={tdStyles}>{type}</td>
+                   <td style={tdStyles}>{amount}</td>
+                   <td style={tdStyles}>{currency}</td>
                 </tr>
                 ))}
-        <tr>
-        </tr>
-        <tr>
-            <td>Withdrawal</td>
-            <td>85</td>
-            <td>USD</td>
-        </tr>
         </tbody>
         </table>
     )
